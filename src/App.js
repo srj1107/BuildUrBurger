daa99106cf3913e52f1e080ae41b2eb3ecc0e89d
srj1107 <srj11052001@gmail.com>
2021-05-31 15:04:27 +0530

    Basic Authentication

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
-
+import Auth from './containers/Auth/Auth';
 
 //class based App component 
 class App extends Component{
@@ -19,6 +19,7 @@ class App extends Component{
             
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders}/>
+            <Route path="/auth" component={Auth} />
             <Route path="/" component={BurgerBuilder} />
           </Switch>
         </Layout>
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -33,7 +33,8 @@ class ContactData extends Component{
                     },
                     value:'',
                     validation:{
-                        required: true
+                        required: true,
+                        isEmail: true
                     },
                     valid: false,
                     touched:false
@@ -47,6 +48,7 @@ class ContactData extends Component{
                     value:'',
                     validation:{
                         required: true
+
                     },
                     valid: false,
                     touched:false
@@ -61,7 +63,8 @@ class ContactData extends Component{
                     validation:{
                         required: true,
                         minLength: 6,
-                        maxLength: 6
+                        maxLength: 6,
+                        isNumeric: true
                     },
                     valid: false,
                     touched:false
@@ -108,6 +111,14 @@ class ContactData extends Component{
         }
         if(rules.maxLength){
             isValid = value.length <=rules.maxLength && isValid;
+        }
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
         }
         return isValid;
     }
