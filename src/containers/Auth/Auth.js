43fc0097647b5882da45ade4aa408b5b825cce7c
srj1107 <srj11052001@gmail.com>
2021-07-02 16:28:18 +0530

    Finishing up.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import classes from "./Auth.css";
 import * as actions from "../../store/actions/index";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
+import {updateObject} from '../../shared/utility';
 
 class Auth extends Component {
   state = {
@@ -72,20 +73,19 @@ class Auth extends Component {
   }
 
   inputChangedHandler = (event, controlName) => {
-    const updatedControls = {
-      ...this.state.controls,
-      [controlName]: {
-        ...this.state.controls[controlName],
+    const updatedControls = updateObject(this.state.controls,{
+      [controlName]: updateObject(this.state.controls[controlName], {
         value: event.target.value,
         valid: this.checkValidity(
           event.target.value,
           this.state.controls[controlName].validation
         ),
-        touched: true,
-      },
-    };
+        touched: true
+      }),
+    })
     this.setState({ controls: updatedControls });
-  };
+  }; 
+
 
   submitHandler = (event) => {
     event.preventDefault();
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,10 +3,8 @@ import {Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
-// import * as actions from '../../store/actions/index';
 
-class Checkout extends Component{
-    
+class Checkout extends Component{   
     
 
     checkoutCancelledHandler =()=>{
